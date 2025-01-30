1113c19807ffb73d8279e43bd88b3eefba3e08a0
Aliasgar Vohra <aliasgar.vohra@rishabhsoft.com>
2025-01-30 22:27:19 +0530

    🔧 : Example app native configurations updated.

--- example/lib/main.dart
+++ example/lib/main.dart
@@ -30,12 +30,11 @@ class _MyAppState extends State<MyApp> {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'Isolate Logger',
       theme: ThemeData(
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-        useMaterial3: true,
       ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      home: const MyHomePage(title: 'Isolate Logger Example'),
     );
   }
 }
@@ -64,66 +63,62 @@ class _MyHomePageState extends State<MyHomePage> {
   }
 
   @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        title: Text(widget.title),
-      ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
-            ),
-            TextButton(
-              onPressed: () => throw (Exception("Fake-Error")),
-              child: const Text("Throw fake-exception"),
-            ),
-            TextButton(
-              onPressed: () => IsolateLogger.instance.clearAllLogs(),
-              child: const Text("Clear logs"),
-            ),
-            TextButton(
-              onPressed: () async {
-                final result = await IsolateLogger.instance.exportLogs(
-                  exportType: LogsExportType.all,
-                );
-                if (result?.isNotEmpty ?? false) {
-                  await Share.shareXFiles([XFile(result!)]);
-                }
-              },
-              child: const Text("Export logs"),
-            ),
-            TextButton(
-              onPressed: () async {
-                final result = await IsolateLogger.instance.exportLogs(
-                  exportType: LogsExportType.all,
-                  logZipFilePrefix: "myPersonalDeviceID_QA",
-                );
-                if (result?.isNotEmpty ?? false) {
-                  await Share.shareXFiles([XFile(result!)]);
-                }
-              },
-              child: const Text("Export logs with prefix"),
-            ),
-            TextButton(
-              onPressed: () => IsolateLogger.instance.dispose(),
-              child: const Text("Dispose isolate"),
-            ),
-          ],
+  Widget build(BuildContext context) => Scaffold(
+        appBar: AppBar(
+          backgroundColor: Theme.of(context).colorScheme.inversePrimary,
+          title: Text(widget.title),
         ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
-    );
-  }
+        body: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+              const Text('You have pushed the button this many times:'),
+              Text(
+                '$_counter',
+                style: Theme.of(context).textTheme.headlineMedium,
+              ),
+              TextButton(
+                onPressed: () => throw (Exception("Fake-Error")),
+                child: const Text("Throw fake-exception"),
+              ),
+              TextButton(
+                onPressed: () => IsolateLogger.instance.clearAllLogs(),
+                child: const Text("Clear logs"),
+              ),
+              TextButton(
+                onPressed: () async {
+                  final result = await IsolateLogger.instance.exportLogs(
+                    exportType: LogsExportType.all,
+                  );
+                  if (result?.isNotEmpty ?? false) {
+                    await Share.shareXFiles([XFile(result!)]);
+                  }
+                },
+                child: const Text("Export logs"),
+              ),
+              TextButton(
+                onPressed: () async {
+                  final result = await IsolateLogger.instance.exportLogs(
+                    exportType: LogsExportType.all,
+                    logZipFilePrefix: "myPersonalDeviceID_QA",
+                  );
+                  if (result?.isNotEmpty ?? false) {
+                    await Share.shareXFiles([XFile(result!)]);
+                  }
+                },
+                child: const Text("Export logs with prefix"),
+              ),
+              TextButton(
+                onPressed: () => IsolateLogger.instance.dispose(),
+                child: const Text("Dispose isolate"),
+              ),
+            ],
+          ),
+        ),
+        floatingActionButton: FloatingActionButton(
+          onPressed: _incrementCounter,
+          tooltip: 'Increment',
+          child: const Icon(Icons.add),
+        ), // This trailing comma makes auto-formatting nicer for build methods.
+      );
 }
